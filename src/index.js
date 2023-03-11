fdb0402c02c7fa5360895cc26dfe54f5f0f2dcc3
rr5050 <rino.rabe@gmail.com>
2023-03-11 17:12:43 +0100

    innitial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send('Home Page')
+})
+
+app.get('/users', (req, res) => {
+	res.send('Users Page')
+})
+
+app.listen(3000, () => console.log('Server Started'))
